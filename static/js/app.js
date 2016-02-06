3805cfa29ff0460d2eff7d701fe77de6829d6e26
ChangJoo-Park <pcjpcj2@gmail.com>
2016-02-06 10:38:59 +0100

    Use factory service for access API

--- static/js/app.js
+++ static/js/app.js
@@ -1,29 +1,65 @@
 var groceryApp = angular.module('groceryApp', ['ngResource']);
 
-groceryApp.controller('GroceryListCtrl', ['$scope', '$http', function($scope, $http){
-    $scope.myWord = "Hello GroceryTracker";
+groceryApp.factory('GroceryService', ['$http', function($http){
+    var urlBase = '/api/groceries';
+    var dataFactory = {};
+    dataFactory.getGroceries = function() {
+        return $http.get(urlBase);
+    };
 
+    dataFactory.getGrocery = function(id) {
+        return $http.get(urlBase + "/" + id);
+    };
+
+    dataFactory.addGrocery = function(data) {
+        return $http.post(urlBase, data);
+    };
+
+    dataFactory.deleteGrocery = function(data) {
+        return $http.delete(urlBase + "/" + data.id);
+    }
+
+    dataFactory.updateGrocery = function(data) {
+        return $http.put(urlBase + "/" + data.id, data);
+    }
+
+    return dataFactory;
+}]);
+
+groceryApp.controller('GroceryListCtrl', ['$scope', '$http', 'GroceryService',
+                                 function($scope, $http, GroceryService){
+    $scope.myWord = "Hello GroceryTracker";
     $scope.newGrocery = {};
     // GET
-    $http.get('/api/groceries').success(function(result) {
-        $scope.groceries = result.data;
-    });
-    // POST
-    $scope.addGrocery = function(){
-        console.log("Start Add Grocery");
-        console.log($scope.newGrocery);
-        $http.post('/api/groceries', $scope.newGrocery).success(function(data){
-            $scope.groceries.push(data);
+    // $scope.groceries = GroceryService.getGroceries();
+    getGroceries();
+    function getGroceries() {
+        GroceryService.getGroceries().success(function(result){
+            $scope.groceries = result.data;
         });
+    }
+
+    // POST
+    function addGrocery() {
+        var data = $scope.newGrocery;
+        data.bought_at = moment(data.bought_at).format("YYYY-MM-DD");
+        GroceryService.addGrocery(data).success(function(){
+            getGroceries();
+        });
+        data = {};
+    }
+    $scope.addGrocery =  function () {
+        addGrocery();
         $scope.newGrocery = {};
     }
+
     // PUT
     $scope.updateGrocery = function(grocery){
-        $http.put('/api/grocery/' + grocery.id, grocery);
+        GroceryService.updateGrocery(grocery);
     }
     // DELETE
     $scope.removeGrocery = function(grocery) {
-        $http.delete('/api/grocery/' + grocery.id).success(function(response){
+        GroceryService.deleteGrocery(grocery).success(function(response){
             $scope.groceries.splice($scope.groceries.indexOf(grocery), 1);
         });
     };
