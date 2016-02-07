0cb345bd997852f42fbc9c07cb930d7b9d72d300
ChangJoo-Park <pcjpcj2@gmail.com>
2016-02-07 11:42:29 +0100

    Format app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,32 +1,32 @@
 var groceryApp = angular.module('groceryApp', ['ngResource', 'ngRoute', 'ngAnimate']);
 
-groceryApp.config(['$routeProvider',function($routeProvider){
+groceryApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
-        when('/', {
-            templateUrl: '../html/grocery/list.html',
-            controller: 'GroceryListCtrl'
-        }).
-        when('/new', {
-            templateUrl: '../html/grocery/new.html',
-            controller: 'GroceryItemAddCtrl'
-        }).
-        when('/:itemId/edit', {
-            templateUrl: '../html/grocery/edit.html',
-            controller: 'GroceryItemEditCtrl'
-        }).
-        when('/about', {
-            templateUrl: '../html/static/about.html',
-            controller: 'AboutCtrl'
-        }).
-      otherwise({
+    when('/', {
+        templateUrl: '../html/grocery/list.html',
+        controller: 'GroceryListCtrl'
+    }).
+    when('/new', {
+        templateUrl: '../html/grocery/new.html',
+        controller: 'GroceryItemAddCtrl'
+    }).
+    when('/:itemId/edit', {
+        templateUrl: '../html/grocery/edit.html',
+        controller: 'GroceryItemEditCtrl'
+    }).
+    when('/about', {
+        templateUrl: '../html/static/about.html',
+        controller: 'AboutCtrl'
+    }).
+    otherwise({
         redirectTo: '/'
-      });
+    });
 }]);
 
-groceryApp.directive('animateOnChange', ['$timeout', function($timeout){
+groceryApp.directive('animateOnChange', ['$timeout', function($timeout) {
     return function(scope, element, attr) {
-        scope.$watch(attr.animateOnChange, function(nv, ov){
-            if (nv!=ov) {
+        scope.$watch(attr.animateOnChange, function(nv, ov) {
+            if (nv != ov) {
                 element.addClass('changed');
                 $timeout(function() {
                     element.removeClass('changed');
@@ -39,23 +39,23 @@ groceryApp.directive('animateOnChange', ['$timeout', function($timeout){
 
 
 groceryApp.filter('unique', function() {
-   return function(collection, keyname) {
-      var output = [],
-          keys = [];
+    return function(collection, keyname) {
+        var output = [],
+            keys = [];
 
-      angular.forEach(collection, function(item) {
-          var key = item[keyname];
-          if(keys.indexOf(key) === -1) {
-              keys.push(key);
-              output.push(item);
-          }
-      });
-      return output;
-   };
+        angular.forEach(collection, function(item) {
+            var key = item[keyname];
+            if (keys.indexOf(key) === -1) {
+                keys.push(key);
+                output.push(item);
+            }
+        });
+        return output;
+    };
 });
 
 
-groceryApp.factory('GroceryService', ['$http', function($http){
+groceryApp.factory('GroceryService', ['$http', function($http) {
     var urlBase = '/api/groceries';
     var dataFactory = {};
     dataFactory.getGroceries = function() {
@@ -83,174 +83,174 @@ groceryApp.factory('GroceryService', ['$http', function($http){
 
 // Controllers
 groceryApp.controller('GroceryListCtrl', ['$scope', '$http', 'GroceryService',
-                                 function($scope, $http, GroceryService){
-    $scope.myWord = "Hello GroceryTracker";
-    $scope.newGrocery = {};
-    $scope.filteredGroceries = {}
-    $('#searchByName').select2({
-        placeholder: "Search By Name",
-        allowClear: true
-    });
-    $('#searchByPlace').select2({
-        placeholder: "Search By Place",
-        allowClear: true
-    });
-    $scope.searchByName = '';
-    $scope.searchByPlace = '';
-    $scope.searchByDate = '';
-
-    $('#searchByName').change(function(){
-        data = $("#searchByName").select2("data")[0];
-        if(data === undefined) {
-            $scope.$apply(function(){
-                $scope.searchByName = '';
-            });
-            return;
-        }
-        data = data.text.trim();
-        $scope.$apply(function(){
-            $scope.searchByName = data;
-        });
-    });
-
-    $('#searchByPlace').change(function(){
-        data = $("#searchByPlace").select2("data")[0];
-        if(data === undefined) {
-            $scope.$apply(function(){
-                $scope.searchByPlace = '';
-            });
-            return;
-        }
-        data = data.text.trim();
-        $scope.$apply(function(){
-            $scope.searchByPlace = data;
-        });
-    });
-
-    $("#datepicker").datepicker({
-        format: "yyyy-mm-dd",
-        weekStart: 1,
-        autoclose: true,
-        todayHighlight: true,
-        clearBtn: true
-    }).on('changeDate', function(e){
-        $scope.$apply(function(){
-            if(e.dates.length === 0) {
-                $scope.searchByDate = '';
-            } else {
-                $scope.searchByDate = moment(e.date).format("YYYY-MM-DD");
-            }
-        });
-    });
-
-    // GET
-    getGroceries();
-    function getGroceries() {
-        GroceryService.getGroceries().success(function(result){
-            $scope.groceries = result.data;
-        });
-    }
-    $scope.refresh = function() {
-        $scope.groceries = {};
-        getGroceries();
-    }
-    // POST
-    function addGrocery() {
-        var data = $scope.newGrocery;
-        data.bought_at = moment(data.bought_at).format("YYYY-MM-DD");
-        GroceryService.addGrocery(data).success(function(){
-            getGroceries();
-        });
-        data = {};
-    }
-    $scope.addGrocery =  function () {
-        addGrocery();
+    function($scope, $http, GroceryService) {
+        $scope.myWord = "Hello GroceryTracker";
         $scope.newGrocery = {};
-    }
+        $scope.filteredGroceries = {};
+        $scope.searchByName = '';
+        $scope.searchByPlace = '';
+        $scope.searchByDate = '';
 
-    // PUT
-    $scope.updateGrocery = function(grocery){
-        GroceryService.updateGrocery(grocery);
-    }
-    // DELETE
-    $scope.removeGrocery = function(grocery) {
-        GroceryService.deleteGrocery(grocery).success(function(response){
-            $scope.groceries.splice($scope.groceries.indexOf(grocery), 1);
-        });
-    };
-
-    // Total Price
-    $scope.totalPrice = function(){
-        if($scope.groceries != null) {
-            var totalPrice = 0;
-            for(var count=0; count<$scope.groceries.length; count++) {
-                totalPrice += $scope.groceries[count].price;
+        $('#searchByName').select2({
+            placeholder: "Search By Name",
+            allowClear: true
+        }).change(function() {
+            data = $("#searchByName").select2("data")[0];
+            if (data === undefined) {
+                $scope.$apply(function() {
+                    $scope.searchByName = '';
+                });
+                return;
             }
-            return totalPrice;
-        }
-    };
+            data = data.text.trim();
+            $scope.$apply(function() {
+                $scope.searchByName = data;
+            });
+        });
+        $('#searchByPlace').select2({
+            placeholder: "Search By Place",
+            allowClear: true
+        }).change(function() {
+            data = $("#searchByPlace").select2("data")[0];
+            if (data === undefined) {
+                $scope.$apply(function() {
+                    $scope.searchByPlace = '';
+                });
+                return;
+            }
+            data = data.text.trim();
+            $scope.$apply(function() {
+                $scope.searchByPlace = data;
+            });
+        });
+        $("#datepicker").datepicker({
+            format: "yyyy-mm-dd",
+            weekStart: 1,
+            autoclose: true,
+            todayHighlight: true,
+            clearBtn: true
+        }).on('changeDate', function(e) {
+            $scope.$apply(function() {
+                if (e.dates.length === 0) {
+                    $scope.searchByDate = '';
+                } else {
+                    $scope.searchByDate = moment(e.date).format("YYYY-MM-DD");
+                }
+            });
+        });
 
-    $scope.totalFilteredPrice = function() {
-        if(!$scope.filtered || $scope.filteredGroceries === undefined) {
-            return;
-        }
-        var sum = 0;
-        angular.forEach($scope.filteredGroceries,function(item){
-            sum +=item.price;
-        })
-        return sum;
-    };
+        // GET
+        getGroceries();
 
-    $scope.filtered = function() {
-        if($scope.searchByName === undefined && $scope.searchByPlace === undefined && $scope.searchByDate === undefined) {
-            return false;
+        function getGroceries() {
+            GroceryService.getGroceries().success(function(result) {
+                $scope.groceries = result.data;
+            });
         }
-        filterLength = getLengthIfExists([$scope.searchByName, $scope.searchByPlace, $scope.searchByDate]);
-        if(filterLength > 0) {
-            return true;
-        } else {
-            return false;
+        $scope.refresh = function() {
+                $scope.groceries = {};
+                getGroceries();
+            }
+            // POST
+        function addGrocery() {
+            var data = $scope.newGrocery;
+            data.bought_at = moment(data.bought_at).format("YYYY-MM-DD");
+            GroceryService.addGrocery(data).success(function() {
+                getGroceries();
+            });
+            data = {};
         }
-    }
+        $scope.addGrocery = function() {
+            addGrocery();
+            $scope.newGrocery = {};
+        }
 
-    function getLengthIfExists(checkStrings) {
-        var length = 0;
-        for (var i = checkStrings.length - 1; i >= 0; i--) {
-            if(checkStrings[i] !== undefined) {
-                length += checkStrings[i].length;
+        // PUT
+        $scope.updateGrocery = function(grocery) {
+                GroceryService.updateGrocery(grocery);
+            }
+            // DELETE
+        $scope.removeGrocery = function(grocery) {
+            GroceryService.deleteGrocery(grocery).success(function(response) {
+                $scope.groceries.splice($scope.groceries.indexOf(grocery), 1);
+            });
+        };
+
+        // Total Price
+        $scope.totalPrice = function() {
+            if ($scope.groceries != null) {
+                var totalPrice = 0;
+                for (var count = 0; count < $scope.groceries.length; count++) {
+                    totalPrice += $scope.groceries[count].price;
+                }
+                return totalPrice;
             }
         };
-        return length;
-    }
 
-}]);
+        $scope.totalFilteredPrice = function() {
+            if (!$scope.filtered || $scope.filteredGroceries === undefined) {
+                return;
+            }
+            var sum = 0;
+            angular.forEach($scope.filteredGroceries, function(item) {
+                sum += item.price;
+            })
+            return sum;
+        };
+
+        $scope.filtered = function() {
+            if ($scope.searchByName === undefined && $scope.searchByPlace === undefined && $scope.searchByDate === undefined) {
+                return false;
+            }
+            filterLength = getLengthIfExists([$scope.searchByName, $scope.searchByPlace, $scope.searchByDate]);
+            if (filterLength > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        function getLengthIfExists(checkStrings) {
+            var length = 0;
+            for (var i = checkStrings.length - 1; i >= 0; i--) {
+                if (checkStrings[i] !== undefined) {
+                    length += checkStrings[i].length;
+                }
+            };
+            return length;
+        }
+
+    }
+]);
 
 groceryApp.controller('GroceryItemEditCtrl', ['$scope', '$routeParams', 'GroceryService', '$location',
-                             function($scope, $routeParams, GroceryService, $location){
-    $scope.itemId = $routeParams.itemId;
-    GroceryService.getGrocery($scope.itemId).success(function(result){
-        $scope.item = result.data;
-    });
-
-    $scope.update = function(item) {
-        GroceryService.updateGrocery($scope.item).success(function(){
-            $location.path('#/');
+    function($scope, $routeParams, GroceryService, $location) {
+        $scope.itemId = $routeParams.itemId;
+        GroceryService.getGrocery($scope.itemId).success(function(result) {
+            $scope.item = result.data;
         });
-    };
-}]);
+
+        $scope.update = function(item) {
+            GroceryService.updateGrocery($scope.item).success(function() {
+                $location.path('#/');
+            });
+        };
+    }
+]);
 
 groceryApp.controller('GroceryItemAddCtrl', ['$scope', 'GroceryService', '$location',
-                             function($scope, GroceryService, $location){
-    $scope.item = {};
-    $scope.add = function () {
-        var item = $scope.item;
-        GroceryService.addGrocery(item).success(function(){
-            $location.path('#/');
-        });
-    };
-}]);
+    function($scope, GroceryService, $location) {
+        $scope.item = {};
+        $scope.add = function() {
+            var item = $scope.item;
+            GroceryService.addGrocery(item).success(function() {
+                $location.path('#/');
+            });
+        };
+    }
+]);
 
 
-groceryApp.controller('AboutCtrl', ['$scope',function($scope){
+groceryApp.controller('AboutCtrl', ['$scope', function($scope) {
 
 }]);
